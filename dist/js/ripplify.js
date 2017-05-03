e54c771c4a00f8a4badf4764ed1d79270a9724fb
Doug Beney <email@dougbeney.com>
2017-05-03 00:29:20 -0400

    Ripplify v.1

---
+++ dist/js/ripplify.js
@@ -0,0 +1,38 @@
+(function($){
+	$(function(){
+		$('.btn-ripple').on('click', function(e){
+			var $div = $('<div class="ripple-effect"></div>');
+			
+			var btnPos = $(this).offset();
+			var mouseX = e.pageX - btnPos.left;
+			var mouseY = e.pageY - btnPos.top;
+			
+			var user_animation_duration = $(this).data('animation-duration');
+			var user_ripple_color = $(this).data('ripple-color');
+			
+
+			if(user_animation_duration == undefined){
+				user_animation_duration = 1000;
+			}
+
+			if(user_ripple_color != undefined){
+				$div.css('background', user_ripple_color);
+			}
+
+			$div.css('animation-duration', user_animation_duration+'ms');
+			console.log('hh ' +$div.css('animation-duration'));
+
+			$div.css({
+				top: mouseY - 25, //Look in the CSS. 25 is half of .ripple-effects width/height 
+				left: mouseX - 25,
+			});
+			
+			$(this).append($div);
+			
+			window.setTimeout(function(){
+		        $div.remove();
+		  }, user_animation_duration);
+		});
+	});
+})(jQuery);
+
